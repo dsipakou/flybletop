92719537e959ef2b3f43e3367ced99e8cd64c69e
dzianis.sipakou <dzianis.sipakou@itechart-group.com>
2016-10-17 18:12:50 +0300

    some changes

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import SearchForm
 
 def index(request):
     inserts = Product.objects.all()
-    return render(request, 'index.html', {'inserts': inserts})
+    form = SearchForm()
+    return render(request, 'index.html', {'inserts': inserts, 'form': form})
 
 def detail(request, product_id):
     product = Product.objects.get(id=product_id)
