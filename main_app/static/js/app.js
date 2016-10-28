a1d76b173606af5f5d6cc6016210d287d7ed6f08
dzianis.sipakou <dzianis.sipakou@itechart-group.com>
2016-10-28 16:20:30 +0300

    image preview updated

--- main_app/static/js/app.js
+++ main_app/static/js/app.js
@@ -3,4 +3,20 @@ $(document).ready(function() {
        $('#imagepreview').attr('src', $(this).find('#imageresource').attr('src'));
        $('#imagemodal').modal('show');
     });
+
+    $(".fancybox-thumb").fancybox({
+		prevEffect	: 'none',
+		nextEffect	: 'none',
+		helpers	: {
+			title	: {
+				type: 'outside'
+			},
+			thumbs	: {
+				width	: 50,
+				height	: 50
+			}
+		}
+	});
+
+	$(".fancybox").fancybox();
 });
